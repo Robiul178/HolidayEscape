17f8f82bd7976c55510aa138c4120eb693dea124
Robiul Islam <robiulislam.me2022@gmail.com>
2022-11-10 00:55:35 +0600

    create review section

--- src/componants/Review/Review.js
+++ src/componants/Review/Review.js
@@ -1,48 +1,64 @@
 import React from 'react';
 import { useState } from 'react';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 const Review = () => {
 
 
-    const [users, setUser] = useState([]);
+    const [user, setUser] = useState({});
 
-    useEffect(() => {
-        fetch('http://localhost:5000/users')
-            .then(res => res.json())
-            .then(data => setUser(data))
-    });
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/users')
+    //         .then(res => res.json())
+    //         .then(data => setUser(data))
+    // });
 
-    const handleAddUser = event => {
+    const handleAddReview = event => {
         event.preventDefault();
-        const name = event.target.name.value;
-        const email = event.target.email.value;
-        const user = { name, email }
-        // console.log(user);
-        fetch('http://localhost:5000/users', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(user)
 
-        })
-            .then(req => req.json())
-            .then(data => {
-                // console.log(data);
-                const newUsers = [...users, data];
-                setUser(newUsers);
-            })
-        event.target.reset()
+        console.log(user)
+
+
+        // const name = event.target.name.value;
+        // const email = event.target.email.value;
+        // const user = { name, email }
+        // // console.log(user);
+        // fetch('http://localhost:5000/users', {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(user)
+
+        // })
+        //     .then(req => req.json())
+        //     .then(data => {
+        //         // console.log(data);
+        //         const newUsers = [...user, data];
+        //         setUser(newUsers);
+        //     })
+        // event.target.reset()
     }
+
+    const handleInputBlur = event => {
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = { ...user };
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
 
     return (
         <div>
             <h2>For Review</h2>
-            <form onSubmit={handleAddUser}>
-                <input type="text" name="name" placeholder='name' />
+            <form onSubmit={handleAddReview}>
+                <input onBlur={handleInputBlur} type="text" name="name" placeholder='name' />
                 <br />
-                <input type="email" name="email" id="" placeholder='email' />
+                <div>
+                    <label for=""></label>
+                    <textarea onBlur={handleInputBlur} class="form-control" type="text" placeholder='Write here!' name="textarea" id="" rows="3"></textarea>
+                </div>
                 <br />
                 <button type="submit">Submit</button>
             </form>
